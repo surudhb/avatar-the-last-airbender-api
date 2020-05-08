39bff2f38581875684c4225f0476a5a72b4fc023
Surudh Bhutani <bsurudh@gmail.com>
2020-05-07 22:54:42 -0400

    span click bug fix

--- public/scripts/collapsible.js
+++ public/scripts/collapsible.js
@@ -6,14 +6,20 @@ function handleClick(e) {
         if(makeActive && input.checked == false) input.parentNode.children[1].click()
         if(!makeActive && input.checked == true) input.parentNode.children[1].click()
     }
-    masterButton.textContent = masterButton.textContent == "Expand All" ? "Collapse All" : "Expand All" 
+    masterButton.textContent = masterButton.textContent == "Expand All" ? "Collapse All" : "Expand All"
+    e.stopPropagation() 
 }
 
 function handleCheck(e) {
-    if(e.target.className != "collapsible") {
-        const text = e.target.innerText.split("\n")
-        const symbol = text[1] == "+" ? "-" : "+"
-        e.target.innerHTML = `<span>${text[0]} <span style=\"float: right;\">${symbol}</span></span>`
+    if(e.target.nodeName == "LABEL" || e.target.nodeName == "SPAN") {
+        if(e.target.nodeName == "LABEL") {
+            const text = e.target.innerText.split("\n")
+            const symbol = text[1] == "+" ? "-" : "+"
+            e.target.innerHTML = `<span>${text[0]} <span style=\"float: right;\">${symbol}</span></span>`
+            e.stopPropagation()
+        } else {
+            e.target.innerHTML = e.target.innerText == "-" ? "+" : "-"
+            e.stopPropagation()
+        }
     }
-    e.stopPropagation()
 }
